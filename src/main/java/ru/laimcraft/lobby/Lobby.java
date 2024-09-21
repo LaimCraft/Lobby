408863f5fbdb6260ef3914c7674820cc643892ae
isaevisa05 <isaevisao55@gmail.com>
2024-09-21 14:41:55 +0300

    Update

---
+++ src/main/java/ru/laimcraft/lobby/Lobby.java
@@ -0,0 +1,16 @@
+package ru.laimcraft.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
